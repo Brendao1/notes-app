450f66eace165a5c99a5e9d4cef7fc43bdfd031c
Brenda <43685607+Brendao1@users.noreply.github.com>
2019-03-20 07:35:35 +0000

    Update user-stories.js

--- user-stories.js
+++ user-stories.js
@@ -6,12 +6,6 @@ As a programmer
 I can create a new note
 So I can record something I need to remember 
 
-it('shows the content of the note', function() {
-    note = new Note("Javascript is my favourite language")
-    note.show()
-    expect(note.content).toEqual("Javascript is my favourite language")
-})
-
 As a programmer
 I can see the full text of an individual note on its own page
 So I can see all the information in the note
