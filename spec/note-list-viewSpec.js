7c95ee07181b96e981c8782faa24def701cc4ede
“Brenda” <odonnell.brenda@gmail.com>
2019-03-20 13:11:53 +0000

    small changes to note list view Spec

--- spec/note-list-viewSpec.js
+++ spec/note-list-viewSpec.js
@@ -4,12 +4,17 @@ function testNoteListView() {
 
     var note1 = ("Ferraris are awesome")
     var note2 = ("Roses are gorgeous")
+    var note3 = ("The Sky is blue")
 
     list1.add(note1)
-    // list1.add(note2)
+    list1.add(note2)
+    list1.add(note3)
 
-    let output = "<p>Ferraris are awesome<br></p>"
-    assert.isTrue(view1.toHTML() === "<p>Ferraris are awesome<br></p>");
+    let output = 
+                "<p>" + note1 + "<br>"
+                + note2 + "<br>"
+                + note3 + "<br></p>";
+    assert.isTrue(view1.toHTML() === output);
 };
 
 testNoteListView();
