23ddc60daa3cfbe990b79bd555412c9b7ef2b5d2
“Brenda” <odonnell.brenda@gmail.com>
2019-03-21 13:11:09 +0000

    puts controller code in a module

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,10 +1,16 @@
-function Controller() {
-    // this.noteModel = noteModel
-    // this.notesView = notesView
-}
+(function(exports) {
 
-Controller.prototype = {
-    howdy: function() {
-        document.getElementById("app").innerHTML = "<p>howdy</p>"
+    function Controller() {
+        // this.note = new Note(text)
+        // this.NoteListView = new NoteListView(NoteList)
+        // this.NoteList = new NoteList()
     }
-}
+    
+    Controller.prototype = {
+        howdy: function() {
+            document.getElementById("app").innerHTML = "<p>howdy</p>"
+        }
+    }
+
+    exports.Controller = Controller;
+}) (this);
